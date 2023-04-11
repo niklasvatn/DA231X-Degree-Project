d6c93f3521d8b81e808133acf1f601307bc5997c
Niklas Vatn <niklas@vatn.se>
2023-04-11 13:05:39 +0000

    feat: add helper script and docs

--- catkin_ws/src/sam_dos/src/sam_dos_node.py
+++ catkin_ws/src/sam_dos/src/sam_dos_node.py
@@ -8,18 +8,12 @@ TOPICS = ['yaw', 'pitch', 'roll']
 PUBLISH_RATE = 10
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--spoof_value', type=float, required=True,
-                        help='Spoof value for the messages.')
-    parser.add_argument('--noise', type=float, required=True,
-                        help='Amount of noise to add to the messages.')
-    args = parser.parse_args()
-
     rospy.init_node("spoof_node", anonymous=True)
     rate = rospy.Rate(PUBLISH_RATE)
-    publishers = [rospy.Publisher(TOPIC_NAME_SPACE + topic, Float64, queue_size=10) for topic in TOPICS]
+    [yaw_publisher, pitch_publisher, roll_publisher] = [rospy.Publisher(TOPIC_NAME_SPACE + topic, Float64, queue_size=10) for topic in TOPICS]
     while not rospy.is_shutdown():
-        value = max(0, Float64(np.random.normal(args.spoof_value, args.noise)))
-        for publisher in publishers:
-            publisher.publish(value)
+        [y, p, r] = [Float64(x) for x in np.random.normal(0, 1, 3)]
+        yaw_publisher.publish(y)
+        roll_publisher.publish(r)
+        pitch_publisher.publish(p)
         rate.sleep()
