b8e20296b895257d08ddb509d8aeb43464b3517d
Niklas Vatn <niklas@vatn.se>
2023-04-11 14:21:25 +0200

    add initial version of orientation spoofing

---
+++ catkin_ws/src/sam_dos/src/sam_dos_node.py
@@ -0,0 +1,25 @@
+import rospy
+import argparse
+from std_msgs.msg import Float64
+import numpy as np
+
+TOPIC_NAME_SPACE = '/sam/dr/'
+TOPICS = ['yaw', 'pitch', 'roll']
+PUBLISH_RATE = 10
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--spoof_value', type=float, required=True,
+                        help='Spoof value for the messages.')
+    parser.add_argument('--noise', type=float, required=True,
+                        help='Amount of noise to add to the messages.')
+    args = parser.parse_args()
+
+    rospy.init_node('spoof_publisher')
+    rate = rospy.Rate(PUBLISH_RATE)
+    publishers = [rospy.Publisher(TOPIC_NAME_SPACE + topic, Float64, queue_size=10) for topic in TOPICS]
+    while not rospy.is_shutdown():
+        value = max(0, Float64(np.random.normal(args.spoof_value, args.noise)))
+        for publisher in publishers:
+            publisher.publish(value)
+        rate.sleep()
